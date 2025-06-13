2f1388499003345535eff05c12921611b6997a1b
Pratham-kanani <p.kanani10a@gmail.com>
2025-06-13 22:50:12 +0530

    Left Sidebar for desktop

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
 import { Outlet } from 'react-router';
+import {SidebarComponent} from '@syncfusion/ej2-react-navigations';
+import { NavItems } from '../../../components';
 
 const AdminLayout = () => {
-  return (
-    <div className='admin-layout'>
-        MobileSidebar
-        <aside className="w-full max-w-[270px] hidden lg:block">Sidebar</aside>
-        <aside className="children">
-            <Outlet />
-        </aside>
-    </div>
-  )
+    return (
+        <div className='admin-layout'>
+            MobileSidebar
+            <aside className="w-full max-w-[270px] hidden lg:block">
+                <SidebarComponent width={'270px'} enableGestures={false}>
+                    <NavItems />
+                </SidebarComponent>
+            </aside>
+            <aside className="children">
+                <Outlet />
+            </aside>
+        </div>
+    )
 }
 
 export default AdminLayout
