cc1df3dde59a87c86e9660b6142e5c0a319be175
Pratham-kanani <p.kanani10a@gmail.com>
2025-06-15 21:32:57 +0530

    Dashboard Layout

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,7 +1,16 @@
+import { Header } from "../../../components"
+
 
 const dashboard = () => {
+  const user = {name:'Pratham'}
   return (
-    <div>dashboard</div>
+    <main className="dashboard wrapper">
+      <Header 
+        title={`Welcome ${user?.name ?? 'Guest'}  `}
+        description="Track activity, trends and popular destinations in real time"
+      />
+      Dashboard Page Contents
+    </main>
   )
 }
 
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,8 +1,7 @@
-import { isAscii } from "buffer";
 import { Link, NavLink } from "react-router";
 import { sidebarItems } from "~/constants";
 import { cn } from "~/lib/utils.";
-const NavItems = () => {
+const NavItems = ({handleClick} : {handleClick? : () => void}) => {
   const user = {
     name: 'Pratham',
     email: 'p.kanani10a@gmail.com',
@@ -23,7 +22,7 @@ const NavItems = () => {
                 {({isActive}: {isActive : boolean}) => (
                   <div className={cn('group nav-item', {
                     'bg-primary-100 !text-white': isActive
-                  })}>
+                  })} onClick={handleClick}>
                     <img src="{icon}" alt="{label}" className={`group-hover:brightness-0 size-5 group-hover:invert ${isActive? 'brightness-0':'text-dark-200'}`}/>
                     {label}
                     </div>
