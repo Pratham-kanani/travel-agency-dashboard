b6fe93aff9802e15030cba212d70da75bf5f33d8
Pratham-kanani <p.kanani10a@gmail.com>
2025-06-22 21:32:47 +0530

    Add Dashboard features

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig, layout, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 
 export default [
@@ -11,4 +11,7 @@ export default [
         route('trips/create', 'routes/admin/create-trip.tsx'),
         route('trips/:tripId', 'routes/admin/trip-detail.tsx')
     ]),
+    layout('routes/root/page-layout.tsx', [
+        index('routes/root/travel-page.tsx')
+    ])
 ] satisfies RouteConfig;
